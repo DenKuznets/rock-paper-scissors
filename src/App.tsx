0f9972c984bb89ec6340a2435c3d4c0169da8e62
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-08-23 16:27:00 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ScoreTab from './components/ScoreTab'
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+
+  return (
+    <>
+      <CssBaseline/>
+      <ScoreTab />
+    </>
+  )
+}
+
+export default App
