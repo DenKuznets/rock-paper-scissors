910f0a3ea9af46a17161b28e15b69da725fcdc90
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-09-20 15:41:09 +0300

    new todo

--- src/TODO.md
+++ src/TODO.md
@@ -2,3 +2,5 @@ animated appearing on the screen of the : userpick, house pick
 
 on win increment counter
 on lose decrement counter
+
+skipped house pick placeholder, dont forget to add it
--- src/components/HousePick/HousePick.test.tsx
+++ src/components/HousePick/HousePick.test.tsx
@@ -1,3 +1,25 @@
+import { renderWithProviders, screen } from "../../ts/utils-for-tests";
+import HousePick, { HOUSE_PICK_TESTIDS } from "./HousePick";
+import { initialState } from "../../app/appSlice";
+import { Roles } from "../../ts/roles";
+
 test("renders correctly", () => {
-    
+    renderWithProviders(<HousePick />, {
+        preloadedState: {
+            app: {
+                ...initialState,
+                houseChoice: Roles.PAPER,
+            },
+        },
+    });
+
+    const housePickContainer = screen.getByTestId(
+        HOUSE_PICK_TESTIDS.HOUSE_PICK_CONTAINER
+    );
+    const housePickText = screen.getByTestId(
+        HOUSE_PICK_TESTIDS.HOUSE_PICK_TEXT
+    );
+
+    expect(housePickContainer).toBeInTheDocument();
+    expect(housePickText).toBeInTheDocument();
 });
