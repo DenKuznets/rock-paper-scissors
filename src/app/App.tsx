da37fe640b2c217812956af4831fac0d099a4626
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-09-22 14:22:59 +0300

    rules btn margin change

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -122,7 +122,7 @@ function App() {
             <Button
                 data-testid={APP_TESTIDS.APP_SHOW_RULES_BUTTON}
                 sx={{
-                    marginTop: "13rem",
+                    marginTop: { xs: "16rem", md: "13rem" },
                     marginRight: { md: "2rem" },
                     marginBottom: { md: "2rem" },
                     position: { md: "absolute" },
