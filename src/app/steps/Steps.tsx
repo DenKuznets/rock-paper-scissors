ec3fd4de16af23b6f2c4f022836c55ac38f1a26b
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-09-27 15:24:48 +0300

    step4 removed

--- src/app/steps/Steps.tsx
+++ src/app/steps/Steps.tsx
@@ -4,14 +4,14 @@ import { useAppDispatch, useAppSelector } from "../reduxHooks";
 import {
     selectShowStep1,
     selectShowStep2,
-    selectShowStep4,
+    selectShowStep3,
     selectUserChoice,
     setShowStep1,
     setShowStep2,
-    setShowStep4,
+    setShowStep3,
 } from "../appSlice";
 import Step2 from "./Step2/Step2";
-import Step3 from "./Step4/Step4";
+import Step3 from "./Step3/Step3";
 
 export const MAIN_TESTIDS = {
     MAIN_CONTAINER: "main-container",
@@ -23,7 +23,7 @@ const Steps = () => {
     const userChoiceState = useAppSelector(selectUserChoice);
     const showStep1 = useAppSelector(selectShowStep1);
     const showStep2 = useAppSelector(selectShowStep2);
-    const showStep3 = useAppSelector(selectShowStep4);
+    const showStep3 = useAppSelector(selectShowStep3);
 
     useEffect(() => {
         const step1 = step1ref.current;
@@ -59,7 +59,7 @@ const Steps = () => {
                 <Step2
                     handleTransitionEnd={() => {
                         dispatch(setShowStep2(false));
-                        dispatch(setShowStep4(true));
+                        dispatch(setShowStep3(true));
                     }}
                 />
             )}
